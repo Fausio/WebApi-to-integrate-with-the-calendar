ae418c45e6e7f616e4eac38fbfe4826733cc6ef2
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-03-29 15:31:11 +0200

    Add project files.

---
+++ TestGoogleCalendarAPI/models/GoogleCalendar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestGoogleCalendarAPI.models
+{
+    public class GoogleCalendar
+    {
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        public   DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
