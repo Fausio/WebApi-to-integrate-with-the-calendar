007ccd6ad83ff95c4aaebb6d7a1d88851f4d1f11
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-03-30 08:23:13 +0200

    CalendarController

---
+++ TestGoogleCalendarAPI/Controllers/CalendarController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestGoogleCalendarAPI.helpers;
+using TestGoogleCalendarAPI.models;
+
+namespace TestGoogleCalendarAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalendarController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGoogleCalendar([FromBody] GoogleCalendar request)
+        {
+            return Ok(await GoogleCalendarHelper.CreateGoogleCalendar(request));
+        }
+    }
+}
