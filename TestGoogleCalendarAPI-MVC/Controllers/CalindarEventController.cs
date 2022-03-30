2e5aea29fe681c45f2c8bf32a8720421c9c35361
Fausio Matsinhe <fausio.matsinhe@kooli.io>
2022-03-30 09:04:47 +0200

    CalindarEventController

---
+++ TestGoogleCalendarAPI-MVC/Controllers/CalindarEventController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestGoogleCalendarAPI_MVC.Controllers
+{
+    public class CalindarEventController : Controller
+    {
+        public IActionResult createForm()
+        {
+            return View();
+        }
+    }
+}
